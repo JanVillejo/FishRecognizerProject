94206648f647a8e1a22c6907c7715a5117a55bae
knsmnth <keanago13@gmail.com>
2026-05-02 08:33:05 +0800

    fix: migrate PermissionAndroid to useCameraPermission, centralized logging

---
+++ src/services/utils/logger.ts
@@ -0,0 +1,24 @@
+/**
+ * Logger Service
+ * Centralized logging to manage console outputs and potentially 
+ * send logs to a remote service or file in the future.
+ */
+
+const LOG_PREFIX = '[FishRecognizer]';
+
+export const logger = {
+  log: (message: string, ...args: any[]) => {
+    console.log(`${LOG_PREFIX} ${message}`, ...args);
+  },
+  warn: (message: string, ...args: any[]) => {
+    console.warn(`${LOG_PREFIX} ${message}`, ...args);
+  },
+  error: (message: string, ...args: any[]) => {
+    console.error(`${LOG_PREFIX} ${message}`, ...args);
+  },
+  debug: (message: string, ...args: any[]) => {
+    if (__DEV__) {
+      console.log(`${LOG_PREFIX} [DEBUG] ${message}`, ...args);
+    }
+  },
+};
